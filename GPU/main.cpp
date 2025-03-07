b3ed6856b3c6bda9f2d9fe880b36e2ec2a4c13e3
Yelyzaveta Kasapien <elizavetakasapen@gmail.com>
2025-03-07 20:26:20 +0100

    DecisionTree with OpenMP (first commit)

---
+++ GPU/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <mlpack/methods/decision_tree/decision_tree.hpp>
+#include <mlpack/core/data/load.hpp>
+#include <armadillo>
+#include <cuda_runtime.h> // CUDA for GPU parallelization
